94b3284d566d367bdc5d77724c005205b861c550
dkettchen <dkettchen@gmail.com>
2024-12-16 15:40:36 +0000

    add file writing of sorted results

--- run_code_working_file.py
+++ run_code_working_file.py
@@ -1,16 +1,29 @@
 from utils.read_txt import read_txt
 from src.collect_key_words import collect_key_words_from_q2
 from src.sort_categories import find_case
+from utils.json_writer import write_json_files
 
 raw_data = read_txt("q2")
 key_word_dict = collect_key_words_from_q2(raw_data)
 
-# male_list = find_case(key_word_dict["man/boy/male"], "male_aligned")
-# non_male_list = find_case(key_word_dict["man/boy/male"], "non_male_aligned")
-# confl_male_list = find_case(key_word_dict["man/boy/male"], "conflicted_male_aligned")
+male_list = find_case(key_word_dict["man/boy/male"], "male_aligned")
+non_male_list = find_case(key_word_dict["man/boy/male"], "non_male_aligned")
+confl_male_list = find_case(key_word_dict["man/boy/male"], "conflicted_male_aligned")
 
-# female_list = find_case(key_word_dict["woman/girl/female"], "female_aligned")
-# non_female_list = find_case(key_word_dict["woman/girl/female"], "non_female_aligned")
+female_list = find_case(key_word_dict["woman/girl/female"], "female_aligned")
+non_female_list = find_case(key_word_dict["woman/girl/female"], "non_female_aligned")
 confl_female_list = find_case(key_word_dict["woman/girl/female"], "conflicted_female_aligned")
 
-print(confl_female_list)
+# print(confl_female_list)
+
+data_dict = {
+    "male_aligned" : male_list,
+    "non_male_aligned" : non_male_list,
+    "conflicted_male_aligned" : confl_male_list,
+
+    "female_aligned" : female_list,
+    "non_female_aligned" : non_female_list,
+    "conflicted_female_aligned" : confl_female_list,
+}
+
+write_json_files(data_dict, "data/cleaned_q2_write_ins/")
