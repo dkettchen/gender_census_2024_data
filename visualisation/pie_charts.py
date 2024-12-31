1e35aa36396bfe48b404db0a3085e40a140e18e1
dkettchen <dkettchen@gmail.com>
2024-12-31 17:47:06 +0100

    add pie chart

--- visualisation/pie_charts.py
+++ visualisation/pie_charts.py
@@ -1,11 +1,24 @@
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
+from visualisation.vis_utils import make_colour_list
 
 def make_pie(input_srs:pd.Series, data_case:str):
     """
     make pie chart
     """
 
-    fig = px.pie(input_srs, values=0, names=input_srs.index)
+    colours = make_colour_list(input_srs.index, "pronouns")
+
+    labels = input_srs.index
+    values = input_srs.values
+    text = [str(number) + "%" for number in input_srs.values]
+
+    fig = go.Figure(data=[
+        go.Pie(labels=labels, values=values, text=text, marker_colors=colours, 
+               textinfo="text+label", textposition="inside")
+    ])
+
+    fig.update_layout(uniformtext_minsize=8, uniformtext_mode="hide")
 
     return fig
