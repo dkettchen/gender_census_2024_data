0bc2ad55b8a5e7cc2c509ecc918262052d351c04
dkettchen <dkettchen@gmail.com>
2025-01-03 14:46:04 +0000

    add list checker helper

---
+++ utils/list_checker.py
@@ -0,0 +1,10 @@
+def is_in_list(input_str:str, reference_list:list):
+    """
+    checks if the input string is contained in the reference list
+
+    if so returns True, otherwise returns False
+    """
+    if input_str in reference_list:
+        return True
+    else: 
+        return False
