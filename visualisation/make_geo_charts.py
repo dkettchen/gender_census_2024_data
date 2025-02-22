2bc016f677c5468b2600988241cf72b20613f69b
dkettchen <dkettchen@gmail.com>
2025-02-22 17:35:28 +0000

    make charts about locations

--- visualisation/make_geo_charts.py
+++ visualisation/make_geo_charts.py
@@ -1,1 +1,99 @@
-# total
+import pandas as pd
+import plotly.graph_objects as go
+from re import sub
+
+def make_geo_charts(input_df:pd.DataFrame, data_case:str):
+    """
+    takes input df from q35
+
+    makes pie chart about the countries most represented among respondants
+    """
+
+    new_srs = input_df.copy().set_index("UserID")["q35_Location"]
+
+    folder = "visualisation/charts/countries_"
+    suffix = "(Gender Census 2024)"
+
+    # fixing list values back to strings smh
+    for i in range(len(new_srs)):
+        item = new_srs.iloc[i]
+        if type(item) != str:
+            new_srs.iloc[i] = str(item)
+
+    # we should be able to just group by the country column 
+    # (which is the only column other than the index)
+    # & count
+    country_srs = new_srs.groupby(by=new_srs.values).count().sort_values(ascending=False)
+
+    # grouping by english speaking vs not english speaking countries
+    if data_case == "english_speaking_countries":
+        # wikipedia says these countries have 50%+ english as their native language
+        english_speaking_countries = [ 
+            
+            "United States",
+            "United Kingdom",
+            "Australia",
+            "New Zealand",
+            "Canada",
+            "Ireland",
+            # and a buncha islands not in our list
+        ]
+
+        # summing
+        english_speakers = 0
+        non_english_speakers = 0
+        not_specified = 0
+        for country in country_srs.index:
+            if country in english_speaking_countries:
+                english_speakers += country_srs[country]
+            elif country in ['Prefer not to say', "['country aggregated']", "None"]:
+                not_specified += country_srs[country]
+            else:
+                non_english_speakers += country_srs[country]
+        
+        # making new srs
+        country_srs = pd.Series(
+            index=["english-speaking", "non-english-speaking", "not specified"],
+            data=[english_speakers, non_english_speakers, not_specified]
+        )
+
+    # grouping together smallest represented countries to remove clutter
+    elif data_case == "total":
+        small_countries = 0
+        new_country_srs = pd.Series()
+        percent_cutoff = 0.5
+        for country in country_srs.index:
+            country_percent = country_srs[country]/len(input_df) * 100
+            if country_percent < percent_cutoff:
+                small_countries += country_srs[country]
+            else:
+                new_country_srs[country] = country_srs[country]
+        new_country_srs[f"other countries (<{percent_cutoff}%)"] = small_countries
+        country_srs = new_country_srs
+
+    # make a pie chart
+    labels = country_srs.index
+    values = country_srs.values
+
+    fig = go.Figure(data=[
+        go.Pie(
+            labels=labels, values=values, 
+            textposition="inside",
+            textinfo="label+percent"
+        )
+    ])
+
+    data_case_edit = sub(r"_"," ", data_case)
+    chart_title = f"Respondants' locations ({data_case_edit}) {suffix}"
+
+    fig.update_layout(
+        uniformtext_minsize=10,
+        uniformtext_mode="hide",
+        title=chart_title
+    )
+
+    fig.write_image(
+        f"{folder}{data_case}.png",
+        height=800,
+        width=800,
+    )
--- visualisation/run_and_make_charts.py
+++ visualisation/run_and_make_charts.py
@@ -2,16 +2,22 @@ from utils.csv_reader import df_from_csv
 from visualisation.make_pronoun_charts import make_pronoun_charts
 from visualisation.make_label_tickbox_charts import make_label_tickbox_charts
 from visualisation.make_crossover_charts import make_pronouns_by_labels
+from visualisation.make_geo_charts import make_geo_charts
 
-# pronouns
-pronoun_df = df_from_csv("data/cleaned_q9_with_new_columns/q9_clean_01.csv")
+# # pronouns
+# pronoun_df = df_from_csv("data/cleaned_q9_with_new_columns/q9_clean_01.csv")
 # make_pronoun_charts(pronoun_df)
 
-# tickbox labels
-tickbox_label_df = df_from_csv("data/cleaned_q1_with_new_columns/q1_clean_01.csv")
+# # tickbox labels
+# tickbox_label_df = df_from_csv("data/cleaned_q1_with_new_columns/q1_clean_01.csv")
 # make_label_tickbox_charts(tickbox_label_df)
 
-# pronouns x tickboxes
-pronouns_x_labels = pronoun_df.join(tickbox_label_df, lsuffix="left", rsuffix="right")
-make_pronouns_by_labels(pronouns_x_labels, "pronouns")
-make_pronouns_by_labels(pronouns_x_labels, "aligned_pronouns")
+# # pronouns x tickboxes
+# pronouns_x_labels = pronoun_df.join(tickbox_label_df, lsuffix="left", rsuffix="right")
+# make_pronouns_by_labels(pronouns_x_labels, "pronouns")
+# make_pronouns_by_labels(pronouns_x_labels, "aligned_pronouns")
+
+# countries
+geo_df = df_from_csv("data/separated_questions/q35_location.csv")
+make_geo_charts(geo_df, "total")
+make_geo_charts(geo_df, "english_speaking_countries")
