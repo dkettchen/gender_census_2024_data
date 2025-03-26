04ae2d6b98844432454b18160e6f737668d0c1c2
dkettchen <dkettchen@gmail.com>
2025-03-26 18:27:54 +0000

    remove comments

--- visualisation/make_survey_source_charts.py
+++ visualisation/make_survey_source_charts.py
@@ -1,20 +1,6 @@
 import pandas as pd
 from visualisation.chart_bar import make_simple_bar
 
-# total, grouped by source
-    # group by labels
-    # count to get totals
-# make a bar chart? or pie chart? (if they're mutually exclusive)
-
-# over time, adding up as time goes on -> can be only websites bc stuff like "my friend send it" 
-# won't have anything to do w each other, unlike sharing on socials
-    # add time stamp (we could do this before inputting into this func already, 
-    # to reuse the read data later)
-    # create running total of relevant labels
-# make line chart
-    # annotate bump from when it was posted on r/tumblr thingy bc those should be close together in time
-    # if u want & it seems relevant/is look-up-able, annotate when it was mentionned on podcasts etc?
-
 def make_source_charts(input_df:pd.DataFrame):
     
     folder = "visualisation/charts/survey_sources_"
@@ -36,6 +22,5 @@ def make_source_charts(input_df:pd.DataFrame):
 
     # biggest categories over time
         # line chart
-
-
-    pass
+        # (optional, my point of "anything other than tumblr is severely underrepresented" 
+        # has already been proven by total)
