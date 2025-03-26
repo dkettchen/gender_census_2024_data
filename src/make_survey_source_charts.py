7fb6905d83af6d2e0d7b4bd6eb0a2a13b6b3c5c5
dkettchen <dkettchen@gmail.com>
2025-03-26 17:32:22 +0000

    move survey origin function to src and make cleaned columns file

---
+++ src/make_survey_source_charts.py
@@ -0,0 +1,175 @@
+import pandas as pd
+from json import load
+from utils.csv_reader import df_from_csv
+
+# TODO:
+    # lgbt category (and several others) currently contain items that are already covered 
+    # by other categories -> fix in og sorting code please
+        # -make separate category for lgbtqia fandom wiki bc it was mentionned multiple times
+        # -remove any other website mentions we already have categories for
+        # -remove word of mouth from queer loved ones mentions too
+        # -keep prides & unspecified groups basically
+    # -check other categories for doubles smh
+
+file_names_websites = {
+    "ai": "AI",
+    "anime feminist": "Anime Feminist (website)",
+    "bluesky": "Bluesky (social media)",
+    "cohost": "Cohost (social media)",
+    "discord": "Discord (messenger/social media)",
+    "dreamwidth": "Dreamwidth (social media)",
+    "eunuch": "Eunuch Archive (forum/social media)",
+    "facebook": "Facebook (social media)",
+    "fetlife" : "FetLife (BDSM social media)",
+    "forum": "any other Forum",
+    "gender census": "Gender Census",
+    "gender reveal": "Tuck Woodstock's Gender Reveal (podcast)",
+    "goodreads": "Goodreads (book website)",
+    "insta": "Instagram (social media)",
+    "irc": "IRC (messenger)",
+    "lemmy": "Lemmy (forum/social media)",
+    "lex": "Lex (queer dating app)",
+    "limonnur": "Limonnur (forum??)",
+    "linkedin": "LinkedIn (social media)",
+    "mastodon": "Fediverse (Mastodon, etc) (social media)",
+    "matrix": "Matrix (software??)",
+    "melonland": "MelonLand (forum)",
+    "neocities": "Neocities (website hosting)",
+    "nonbinary wiki": "Nonbinary Wiki (wiki website)",
+    "patreon": "Patreon (crowdfunding website)",
+    "pillowfort": "Pillowfort (social media)",
+    "pinterest": "Pinterest (social media)",
+    "pronouns page": "Pronoun Page (website)",
+    "quotev": "Quotev (poll website)",
+    "reddit": "Reddit (social media)",
+    "scratch": "Scratch (website)",
+    "signal": "Signal (messenger)",
+    "slack": "Slack (messenger)",
+    "snapchat": "Snapchat (messenger)",
+    "spacehey": "Spacehey (social media)",
+    "substack": "Substack (newsletter website)",
+    "t4t": "T4T (dating app/social media)",
+    "teams": "Teams (messenger)",
+    "telegram": "IM group (Telegram, WhatsApp, etc) (messenger)",
+    "threads": "Threads (social media)",
+    "tiktok": "TikTok (social media)",
+    "tumblr": "Tumblr (social media)",
+    "twitch": "Twitch (live-streaming website)",
+    "twitter": "Twitter (social media)",
+    "wattpad": "Wattpad (writing-publishing/fanfic website)",
+    "whatsapp": "IM group (Telegram, WhatsApp, etc) (messenger)",
+    "wikipedia": "Wikipedia (wiki website)",
+    "yikyak": "Yik Yak (social media)",
+    "youtube": "YouTube (social media)",
+    "duckduckgo": "DuckDuckGo (search engine)",
+    "google": "Google (search engine)"
+}
+file_names_word_of_mouth = [
+    "word of mouth",
+    "clinician",
+    "family",
+    "friend",
+    "partner",
+]
+file_names_locations = {
+    "lgbt": "LGBT+ group/etc",
+    "school": "School/Uni",
+    "work": "Work",
+}
+
+# util
+def read_all_source_write_ins():
+    """
+    reads from all json files in data/cleaned_q37_write_ins/ folder
+
+    returns a dict with the file names as the keys and the contents as the list value
+    """
+
+    data_dict = {}
+
+    all_file_names = list(file_names_websites.keys()) + list(file_names_locations.keys()) + file_names_word_of_mouth
+
+    for item in all_file_names:
+        filepath = f"data/cleaned_q37_write_ins/{item}.json"
+        with open(filepath, "r") as json_file:
+            read_file = load(json_file)
+        
+        data_dict[item] = read_file
+
+    return data_dict
+
+def make_survey_source_charts(input_df:pd.DataFrame):
+
+    # get sorted bits from files
+    data_dict = read_all_source_write_ins()
+
+    # copying input df
+    source_df = input_df.copy().set_index("UserID")#["q37_Survey_Origin"]
+
+    source_df["origin"] = "Other"
+
+    # websites first
+    for key in file_names_websites:
+        for item in data_dict[key]:
+            source_df["origin"] = source_df["origin"].mask(
+                (source_df["origin"] == "Other") & (
+                source_df["q37_Survey_Origin"] == item),
+                other=file_names_websites[key]
+            )
+            source_df["origin"] = source_df["origin"].mask(
+                (source_df["origin"] != file_names_websites[key]) & (
+                source_df["q37_Survey_Origin"] == item),
+                other=source_df["origin"] + f" & {file_names_websites[key]}"
+            )
+    
+    # offline locations second
+    for key in file_names_locations:
+        for item in data_dict[key]:
+            source_df["origin"] = source_df["origin"].mask(
+                (source_df["origin"] == "Other") & (
+                source_df["q37_Survey_Origin"] == item),
+                other=file_names_locations[key]
+            )
+            source_df["origin"] = source_df["origin"].mask(
+                (source_df["origin"] != file_names_locations[key]) & (
+                source_df["q37_Survey_Origin"] == item),
+                other=source_df["origin"] + f" & {file_names_locations[key]}"
+            )
+
+    for key in file_names_word_of_mouth:
+        for item in data_dict[key]:
+            source_df["origin"] = source_df["origin"].mask(
+                (source_df["origin"] == "Other") & (
+                source_df["q37_Survey_Origin"] == item),
+                other="Word of Mouth"
+            )
+    
+    #print(source_df.tail(40))
+    return source_df
+
+    # adjust input df to be useable
+        # how to categorise ones that have two listed?
+        # read from cleaned files?? -> that have sorted the repeats
+        # replace repeats with common label
+
+    # total, grouped by source
+        # group by labels
+        # count to get totals
+    # make a bar chart? or pie chart? (if they're mutually exclusive)
+
+    # over time, adding up as time goes on -> can be only websites bc stuff like "my friend send it" 
+    # won't have anything to do w each other, unlike sharing on socials
+        # add time stamp (we could do this before inputting into this func already, 
+        # to reuse the read data later)
+        # create running total of relevant labels
+    # make line chart
+        # annotate bump from when it was posted on r/tumblr thingy bc those should be close together in time
+        # if u want & it seems relevant/is look-up-able, annotate when it was mentionned on podcasts etc?
+
+
+if __name__ == "__main__":
+    source_df = df_from_csv("data/separated_questions/q37_how_did_you_find_survey.csv")
+
+    new_df = make_survey_source_charts(source_df)
+
+    new_df.to_csv(path_or_buf="data/cleaned_q37_with_new_columns/q37_clean_01.csv")
--- visualisation/run_and_make_charts.py
+++ visualisation/run_and_make_charts.py
@@ -1,11 +1,14 @@
 from utils.csv_reader import df_from_csv
 from visualisation.make_pronoun_charts import make_pronoun_charts
 from visualisation.make_label_tickbox_charts import make_label_tickbox_charts
-from visualisation.make_crossover_charts import make_pronouns_by_labels, make_non_english_pronouns
+from visualisation.make_crossover_charts import (
+    make_pronouns_by_labels, 
+    make_non_english_pronouns,
+)
 from visualisation.make_geo_charts import make_geo_charts
 
 # # pronouns
-pronoun_df = df_from_csv("data/cleaned_q9_with_new_columns/q9_clean_01.csv")
+# pronoun_df = df_from_csv("data/cleaned_q9_with_new_columns/q9_clean_01.csv")
 # make_pronoun_charts(pronoun_df)
 
 # # tickbox labels
@@ -17,8 +20,8 @@ pronoun_df = df_from_csv("data/cleaned_q9_with_new_columns/q9_clean_01.csv")
 # make_pronouns_by_labels(pronouns_x_labels, "pronouns")
 # make_pronouns_by_labels(pronouns_x_labels, "aligned_pronouns")
 
-# countries
-geo_df = df_from_csv("data/separated_questions/q35_location.csv")
-make_geo_charts(geo_df, "total")
-make_geo_charts(geo_df, "english_speaking_countries")
-make_non_english_pronouns(pronoun_df, geo_df)
+# # countries
+# geo_df = df_from_csv("data/separated_questions/q35_location.csv")
+# make_geo_charts(geo_df, "total")
+# make_geo_charts(geo_df, "english_speaking_countries")
+# make_non_english_pronouns(pronoun_df, geo_df)
