249b4a26426c90271240a5975ce09c3b80cb7ab4
dkettchen <dkettchen@gmail.com>
2025-12-15 09:20:45 +0000

    make new colour file

---
+++ visualisation/vis_utils/colour_palettes.py
@@ -0,0 +1,52 @@
+colours = {
+    "five_categories": {
+        "cis": "black",
+        "unspecified trans/cis status": "silver",
+        "unspecified trans direction": "white",
+        "transmasc": "skyblue",
+        "transfemme": "hotpink",
+    },
+    "pronouns": {
+        "she user":"deeppink",
+        "he user":"royalblue",
+        "they user":"purple",
+        "it user":"green",
+        "neopronoun user":"yellow",
+        "any user":"black",
+
+        "questioning":"silver",
+        "avoid pronouns/name as pronoun":"grey",
+
+        "questioning only":"silver",
+        "avoid pronouns/use name only":"grey",
+
+        "she only":"deeppink",
+        "he only":"royalblue",
+        "they only":"purple",
+        "it only":"green",
+        "neopronoun only":"yellow",
+
+        "she/they":"mediumvioletred",
+        "he/they":"slateblue",
+        "she/he":"violet",
+        "she/he/they any":"black",
+
+        "she/it":"olive",
+        "he/it":"lightseagreen ",
+        "they/it":"saddlebrown",
+        
+        "she/[neo]":"orange",
+        "he/[neo]":"limegreen",
+        "they/[neo]":"chocolate",
+
+        "she/it/[neo]":"tan",
+        "he/it/[neo]":"darkseagreen",
+        "they/it/[neo]":"darkgoldenrod",
+        "it/[neo]":"greenyellow",
+    },
+    "alignments": {
+        "female-aligned":"mediumvioletred",
+        "male-aligned":"slateblue",
+        "unaligned":"purple",
+    },
+}
